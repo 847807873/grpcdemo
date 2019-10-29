4730e0478ae57eab803f11eafa281eaf01802102
郭艳帅 <847807873@qq.com>
2019-10-29 17:59:31 +0800

    第一次提交

---
+++ client/clientmain.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"grpcdemo/test"
+	"log"
+	"os"
+)
+
+func main() {
+
+	//建立连接grpc服务
+
+	conn,err := grpc.Dial("127.0.0.1:8028",grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect : %v",err)
+	}
+	// 函数结束时关闭链接
+	defer conn.Close()
+
+	//创建waiter服务的客户端
+	t := test.NewWaiterClient(conn)
+
+	//模拟请求数据
+	res := "test123"
+
+	if len(os.Args)>1{
+		res = os.Args[1]
+	}
+
+	// 调用grps接口
+	rt ,err := t.DoMD5(context.Background(),&test.Req{JsonStr:res})
+
+	if err !=nil{
+		log.Fatalf("colud not greet :%v",err)
+	}
+
+	log.Printf("服务端响应：%s",rt.BackJson)
+
+
+}
---
+++ server/servermain.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"crypto/md5"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"grpcdemo/test"
+	"log"
+	"net"
+)
+
+type server struct {
+
+}
+
+func (s *server) DoMD5(ctx context.Context,in *test.Req) (*test.Res,error) {
+	fmt.Println("MD5方法请求JSON:"+in.JsonStr)
+
+	return &test.Res{BackJson:"MD5:"+fmt.Sprintf("%x",md5.Sum([]byte(in.JsonStr)))},nil
+}
+
+
+func main()  {
+	//监听所有网卡8028端口的TCP连接
+	lis, err := net.Listen("tcp",":8028")
+	if err!=nil {
+		log.Fatalf("监听失败：%v",err)
+	}
+	//创建gRPC服务
+	s := grpc.NewServer()
+	/*
+	* 注册接口服务
+	* 以定义proto时的service为单位注册，服务中可以有多个方法
+	* （proto编译时会为每个service生成Register***Server方法
+	*  包.注册服务方法（grpc服务实例，包含接口方法的结构体【指针】）
+	 */
+	test.RegisterWaiterServer(s,&server{})
+
+	/*
+	*  如果有可以注册多个接口服务，结构体要实现对应接口方法
+	* user.RegisterLoginServer(s,&server{})
+	*
+	*/
+	reflection.Register(s)
+
+	err = s.Serve(lis)
+	if err !=nil{
+		log.Fatalf("failed to serve: %v",err)
+	}
+}
+
